dc4d401a0ec2c98c58002fb2030f279a5d8bd7f6
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2021-01-14 17:25:35 -0500

    passed unit tests

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+  constructor(name, id, email, officeNo) {
+    super(name, id, email);
+
+    this.officeNumber = officeNo;
+  }
+
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+
+  getRole() {
+    return "Manager";
+  }
+}
+
+module.exports = Manager;
